5645ca15e2ead3a5d97906625c38ecb4fed3100b
Arjav Jain <jarjav2005@gmail.com>
2025-02-20 14:34:32 +0530

    Mongo

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const authRouter = require('./routes/auth.routes.js')
 const userRouter = require('./routes/user.routes.js')
 const app = express()
+const MongoUrl = 'mongodb+srv://aashwasan445:2FXuldBckjw2mOTn@aashwasan.r9ia2.mongodb.net/?retryWrites=true&w=majority&appName=aashwasan';
 
+main()
+.then(()=>{console.log("connection spoted")})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(MongoUrl);
+}
 
 
 app.use(express.json())
